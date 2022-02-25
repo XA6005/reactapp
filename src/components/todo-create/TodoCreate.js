4eedfe714eb5866fbca1c23e0bc7de7c03888e60
Nur Allisia Rahmasari <nur.rahmasari@ist.id>
2022-02-25 08:05:07 +0700

    done todo list with dummy

---
+++ src/components/todo-create/TodoCreate.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './TodoCreate.css'
+
+const TodoCreate = (props) =>{
+    const [getInputTodo,setInputTodo] = useState('')
+
+    const handleInputTodo = (event) =>{
+            setInputTodo(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const newTodo = {
+            id: Math.floor(Math.random() *100) +1,
+            title: getInputTodo
+        }
+
+        props.onCreateTodo(newTodo)
+
+        setInputTodo('') //Supaya value input kosong 
+    }
+    return(
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input type='text' value={getInputTodo} onChange={handleInputTodo}/>
+            <button type='submit'>Add</button>
+        </form>
+    )
+}
+
+export default TodoCreate;
